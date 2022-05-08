ad4659bd0d96fa6eccedbe7732edbe934167da4e
DigiWorm <aus343@gmail.com>
2022-05-07 23:36:48 -0500

    Improved Mobile Support

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,12 @@ import Project from './components/project/Project';
 function App() {
     return (
         <div className="container-fluid">
-            <div className="row align-items-center">
-                <div className="col-md-6 slide-up">
-                    <div className="ps-5">
+            <div className="row align-items-center mt-lg-5 mt-md-4">
+                <div className="col-lg-4 col-md-6 offset-lg-1 order-md-2 slide-up">
+                    <Profile />
+                </div>
+                <div className="col-md-6 slide-up-delayed">
+                    <div className="ps-5 mt-3 mb-5 pe-5">
                         <h1>Hi, I'm <b>Austin Kugler</b></h1>
                         <h2 className="mt-5">
                             I'm a junior software developer with experience in <a href="https://nodejs.org/en/" className="text-success">Node</a>
@@ -19,12 +22,9 @@ function App() {
                         </h2>
                     </div>
                 </div>
-                <div className="col-md-6 slide-up-delayed">
-                    <Profile />
-                </div>
             </div>
             <div className="fade-in">
-                <div className="row">
+                <div className="row mt-4">
                     <div className="col-md-12">
                         <h1 className="text-center"><b>Projects</b></h1>
                     </div>
@@ -109,6 +109,20 @@ function App() {
                             link="https://github.com/DigiWorm0/DigiWorm0.github.io" />
                     </div>
                     <div className="col-md-4">
+                    </div>
+                </div>
+                <div className="row mb-2 mt-1">
+                    <div className="col-sm-12 text-center pe-3">
+                        <a className="btn text-light" href="https://github.com/DigiWorm0">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-github" viewBox="0 0 16 16">
+                                <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+                            </svg>
+                        </a>
+                        <a className="btn text-light" href="mailto:aus343@gmail.com">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-envelope-fill" viewBox="0 0 16 16">
+                                <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z" />
+                            </svg>
+                        </a>
                     </div>
                 </div>
             </div>
--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -3,7 +3,7 @@ import './Profile.css';
 
 function Profile() {
     return (
-        <div className="m-5 p-5">
+        <div className="mt-md-5 mt-3 mb-5 ps-2 pe-2">
             <img src="/profile.jpg" alt="Profile" className="Profile" />
         </div>
     );
